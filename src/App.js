2253c7d86e07ca5f8962fe5077b865d9455b0a7c
mochamadramdhan <mochamadramdhan241200@gmail.com>
2023-03-07 21:36:01 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { Typography, Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Learn React
+        <Typography sx={{ fontWeight: 'bold' }}>ABOUT</Typography>
+        <Typography sx={{ fontWeight: 'bold' }}>PRICING</Typography>
+        <Typography sx={{ fontWeight: 'bold' }}>CONTACT</Typography>
+        <Button variant="contained">Contained</Button>
       </header>
+      <div className='aaa'>
+        <Typography>CONTACT</Typography>
+      </div>
     </div>
   );
 }
