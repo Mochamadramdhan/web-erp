e5f7336fb3ae3bd9cb8264557bae6da36e0ac4a5
mochamadramdhan <mochamadramdhan241200@gmail.com>
2023-03-08 09:55:18 +0700

    add collors.js and upadte App bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import './App.css';
-import { Typography, Button } from '@mui/material';
+import { Typography, Button, AppBar, Toolbar } from '@mui/material';
+import React, { useState } from 'react';
+import { colors } from './theme/colors';
+
+const menuItems = [
+  { id: 1, label: 'ABOUT' },
+  { id: 2, label: 'PRICE' },
+  { id: 3, label: 'CONTACT' },
+];
 
 function App() {
+  const [hoveredItem, setHoveredItem] = useState(null);
+
+  const handleMouseEnter = (itemName) => {
+    setHoveredItem(itemName);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Typography sx={{ fontWeight: 'bold' }}>ABOUT</Typography>
-        <Typography sx={{ fontWeight: 'bold' }}>PRICING</Typography>
-        <Typography sx={{ fontWeight: 'bold' }}>CONTACT</Typography>
-        <Button variant="contained">Contained</Button>
-      </header>
-      <div className='aaa'>
+      <AppBar position="fixed" sx={{ backgroundColor: colors.backroud1 }} component="nav">
+        <Toolbar sx={{ gap: '10%', justifyContent: 'center' }}>
+          {menuItems.map((item) => (
+            <Typography key={item.id}>
+              <span
+                onMouseEnter={() => handleMouseEnter(item.label)}
+                onMouseLeave={handleMouseLeave}
+                style={{
+                  color: hoveredItem === item.label ? colors.main : colors.teks,
+                  cursor: 'pointer',
+                  fontWeight: hoveredItem === item.label ? 'bold' : 'normal',
+                }}
+              >
+                {item.label}
+              </span>
+            </Typography>
+          ))}
+          <Button
+            variant="outlined"
+            sx={{ paddingX: 5, fontWeight: 'bold', color: colors.main, borderColor: colors.main }}
+          >
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <div className="aaa">
         <Typography>CONTACT</Typography>
       </div>
     </div>
---
+++ src/theme/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    main: '#4285f4',
+    teks:'#000000',
+    backroud1:' #FFFFFF'
+    
+}
